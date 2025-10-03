beff6beda60dcdd9c2b8e2d5a045a08d92bd68db
MasterSharp <marco.delpretehome25@outlook.com>
2025-10-03 09:43:59 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace DiscordMultiTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            label1 = new Label();
+            tabPage1 = new TabPage();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            button11 = new Button();
+            button10 = new Button();
+            button9 = new Button();
+            tabControl1 = new TabControl();
+            label2 = new Label();
+            label5 = new Label();
+            checkBox1 = new CheckBox();
+            tabPage1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(292, 42);
+            button1.TabIndex = 0;
+            button1.Text = "Load Discord Rich Presence";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += Button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(12, 60);
+            button2.Name = "button2";
+            button2.Size = new Size(292, 42);
+            button2.TabIndex = 1;
+            button2.Text = "Load Discord Bot Python";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += Button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(12, 204);
+            button3.Name = "button3";
+            button3.Size = new Size(292, 42);
+            button3.TabIndex = 2;
+            button3.Text = "Open Discord Developer Portal";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += Button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.Location = new Point(12, 108);
+            button4.Name = "button4";
+            button4.Size = new Size(292, 42);
+            button4.TabIndex = 3;
+            button4.Text = "Inject DLL";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += Button4_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.Location = new Point(12, 156);
+            button5.Name = "button5";
+            button5.Size = new Size(292, 42);
+            button5.TabIndex = 4;
+            button5.Text = "ViaDiscord";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += Button5_Click;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button6.ForeColor = Color.Red;
+            button6.Location = new Point(12, 348);
+            button6.Name = "button6";
+            button6.Size = new Size(292, 42);
+            button6.TabIndex = 5;
+            button6.Text = "Self Destroy";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += Button6_Click;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button7.ForeColor = Color.Red;
+            button7.Location = new Point(12, 300);
+            button7.Name = "button7";
+            button7.Size = new Size(292, 42);
+            button7.TabIndex = 6;
+            button7.Text = "Destroy Discord Client";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += Button7_Click;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button8.Location = new Point(12, 252);
+            button8.Name = "button8";
+            button8.Size = new Size(292, 42);
+            button8.TabIndex = 7;
+            button8.Text = "Misc";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += Button8_Click;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 498);
+            label1.Name = "label1";
+            label1.Size = new Size(292, 109);
+            label1.TabIndex = 8;
+            label1.Text = "DiscordMultiTool V1.0.0\r\n";
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(checkBox1);
+            tabPage1.Controls.Add(label5);
+            tabPage1.Controls.Add(textBox2);
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Controls.Add(button11);
+            tabPage1.Controls.Add(button10);
+            tabPage1.Controls.Add(button9);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(744, 567);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Settings";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(434, 132);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(304, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(305, 132);
+            label4.Name = "label4";
+            label4.Size = new Size(101, 21);
+            label4.TabIndex = 5;
+            label4.Text = "Target Server";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(305, 36);
+            label3.Name = "label3";
+            label3.Size = new Size(123, 21);
+            label3.TabIndex = 4;
+            label3.Text = "Discord Account";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(434, 36);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(304, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // button11
+            // 
+            button11.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button11.Location = new Point(30, 225);
+            button11.Name = "button11";
+            button11.Size = new Size(232, 33);
+            button11.TabIndex = 2;
+            button11.Text = "Inject Discord Patch";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += Button11_Click;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button10.Location = new Point(30, 126);
+            button10.Name = "button10";
+            button10.Size = new Size(232, 33);
+            button10.TabIndex = 1;
+            button10.Text = "Change Theme";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += Button10_Click;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button9.Location = new Point(30, 30);
+            button9.Name = "button9";
+            button9.Size = new Size(232, 33);
+            button9.TabIndex = 0;
+            button9.Text = "Color GUI";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += Button9_Click;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Location = new Point(310, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(752, 595);
+            tabControl1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(430, 8);
+            label2.Name = "label2";
+            label2.Size = new Size(628, 25);
+            label2.TabIndex = 10;
+            label2.Text = "DiscordMultiTool by MasterSharp and itelcan3 for MasterSharp Team LLC.";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(305, 231);
+            label5.Name = "label5";
+            label5.Size = new Size(103, 21);
+            label5.TabIndex = 7;
+            label5.Text = "Save Settings";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(434, 233);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(124, 19);
+            checkBox1.TabIndex = 8;
+            checkBox1.Text = "Auto Save Settings";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1074, 616);
+            Controls.Add(label2);
+            Controls.Add(tabControl1);
+            Controls.Add(label1);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "DiscordMultiTool";
+            Load += Form1_Load;
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabControl1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Label label1;
+        private TabPage tabPage1;
+        private Button button10;
+        private Button button9;
+        private TabControl tabControl1;
+        private Label label2;
+        private Button button11;
+        private TextBox textBox2;
+        private Label label4;
+        private Label label3;
+        private TextBox textBox1;
+        private CheckBox checkBox1;
+        private Label label5;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,224 @@
+using System;
+using System.Diagnostics;   
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Design;
+using System.IO;
+
+namespace DiscordMultiTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.checkbox == "True")
+            {
+                checkBox1.Checked = true;
+            }
+            else
+            {
+                checkBox1.Checked = false;
+            }
+            this.BackColor = SystemColors.Control;
+            tabControl1.BackColor = SystemColors.Control;
+            tabPage1.BackColor = Color.White;
+            button1.BackColor = SystemColors.Control;
+            button2.BackColor = SystemColors.Control;
+            button3.BackColor = SystemColors.Control;
+            button4.BackColor = SystemColors.Control;
+            button5.BackColor = SystemColors.Control;
+            button6.BackColor = SystemColors.Control;
+            button7.BackColor = SystemColors.Control;
+            button8.BackColor = SystemColors.Control;
+            button9.BackColor = SystemColors.Control;
+            button10.BackColor = SystemColors.Control;
+            label1.BackColor = SystemColors.Control;
+            label2.BackColor = SystemColors.Control;
+            button10.Text = "Change Theme: Modern";
+            // Ottieni informazioni sul processo corrente DEBUG
+            Process processo = Process.GetCurrentProcess();
+            string nomeProcesso = processo.ProcessName;
+            label1.Text = $"Discord Multi Tool V1.0.0\nThis Process: {nomeProcesso}\nDiscordMultiTool/MASTER\nTranslated by itelcan3\nhttps://github.com/CodeSharp3210";
+            string cartella = @"C:\Users\" + Environment.UserName + @"\DiscordMultiTool";
+
+            if (checkBox1.Checked)
+            {
+                Properties.Settings.Default.checkbox = "True";
+                textBox1.Text = Properties.Settings.Default.textbox1;
+                textBox2.Text = Properties.Settings.Default.textbox2;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+
+            }
+
+            // Controlla se la cartella esiste, altrimenti la crea e ci mette dentro i file necessari
+            Properties.Settings.Default.textbox1 = textBox1.Text;
+            Properties.Settings.Default.textbox2 = textBox2.Text;
+            Properties.Settings.Default.Save();
+            string contenutoLog = $"DiscordMultiTool injected.\n{textBox1.Text}\n{textBox2.Text}";
+            if (Directory.Exists(cartella))
+            {
+                if (!File.Exists(cartella + @"\log.txt"))
+                {
+                    File.WriteAllText(cartella + @"\log.txt", contenutoLog);
+                }
+                else
+                {
+                    ;
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(cartella);
+            }
+        }
+
+
+        // GUI & impostazioni programma
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            ColorDialog COLORGUI = new ColorDialog();
+            if (COLORGUI.ShowDialog() == DialogResult.OK)
+            {
+                this.ForeColor = COLORGUI.Color;
+                label1.ForeColor = COLORGUI.Color;
+                label2.ForeColor = COLORGUI.Color;
+                button1.ForeColor = COLORGUI.Color;
+                button2.ForeColor = COLORGUI.Color;
+                button3.ForeColor = COLORGUI.Color;
+                button4.ForeColor = COLORGUI.Color;
+                button5.ForeColor = COLORGUI.Color;
+                button6.ForeColor = COLORGUI.Color;
+                button7.ForeColor = COLORGUI.Color;
+                button8.ForeColor = COLORGUI.Color;
+                tabControl1.ForeColor = COLORGUI.Color;
+                tabPage1.ForeColor = COLORGUI.Color;
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            string url = "https://discord.com/developers/applications";
+            Process.Start(new ProcessStartInfo { FileName = url, UseShellExecute = true });
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            Process[] ProcessoDiscord = Process.GetProcessesByName("Discord");
+            try
+            {
+                ProcessoDiscord[0].Kill();
+            }
+            catch (Exception)
+            {
+                ;
+            }
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            Process processo = Process.GetCurrentProcess();
+            processo.Kill();
+
+        }
+
+        private void Button10_Click(object sender, EventArgs e)
+        {
+
+            if (button10.Text == "Change Theme: Modern")
+            {
+                button10.Text = "Change Theme: Classic";
+                this.BackColor = Color.FromArgb(54, 57, 63);
+                tabControl1.BackColor = Color.FromArgb(47, 49, 54);
+                tabPage1.BackColor = Color.FromArgb(47, 49, 54);
+                button1.BackColor = Color.FromArgb(54, 57, 63);
+                button2.BackColor = Color.FromArgb(54, 57, 63);
+                button3.BackColor = Color.FromArgb(54, 57, 63);
+                button4.BackColor = Color.FromArgb(54, 57, 63);
+                button5.BackColor = Color.FromArgb(54, 57, 63);
+                button6.BackColor = Color.FromArgb(54, 57, 63);
+                button7.BackColor = Color.FromArgb(54, 57, 63);
+                button8.BackColor = Color.FromArgb(54, 57, 63);
+                button9.BackColor = Color.FromArgb(54, 57, 63);
+                button10.BackColor = Color.FromArgb(54, 57, 63);
+                label1.BackColor = Color.FromArgb(54, 57, 63);
+                label2.BackColor = Color.FromArgb(54, 57, 63);
+            }
+            else
+            {
+                button10.Text = "Change Theme: Modern";
+                this.BackColor = SystemColors.Control;
+                tabControl1.BackColor = SystemColors.Control;
+                tabPage1.BackColor = Color.White;
+                button1.BackColor = SystemColors.Control;
+                button2.BackColor = SystemColors.Control;
+                button3.BackColor = SystemColors.Control;
+                button4.BackColor = SystemColors.Control;
+                button5.BackColor = SystemColors.Control;
+                button6.BackColor = SystemColors.Control;
+                button7.BackColor = SystemColors.Control;
+                button8.BackColor = SystemColors.Control;
+                button9.BackColor = SystemColors.Control;
+                button10.BackColor = SystemColors.Control;
+                label1.BackColor = SystemColors.Control;
+                label2.BackColor = SystemColors.Control;
+            }
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Telegram Features is in currently development.\nPlss follow me on GitHub to have last updates");
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            Process[] ProcessoDiscord = Process.GetProcessesByName("Discord");
+            if (ProcessoDiscord.Length == 0)
+            {
+                string discordID = ProcessoDiscord[0].Id.ToString();
+                MessageBox.Show($"Discord Multi Tool is already connected with your Discord Client\nProcess Name: {ProcessoDiscord}\nProcess PID: {discordID}");
+            }
+            else
+            {
+                ;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Form3 form3 = new Form3();
+            form3.Show();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Form4 form4 = new Form4();
+            form4.Show();
+        }
+
+        private void Button11_Click(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcessesByName("explorer");
+            foreach (Process process in processes)
+            {
+                process.Kill();
+            }
+            Thread.Sleep(2000);
+            Process.Start("explorer.exe");
+        }
+    }
+}
